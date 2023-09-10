b0c71a3c8a65cbdb1f19b03300c22f6ed67c10fc
CodWurl <46231725+CodWurl@users.noreply.github.com>
2023-09-10 13:38:37 -0400

    modified components, css, appjs and indexjs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
 
-
-export default function Home () {
-    return(
+export default function Home() {
+    return (
         <div className="homepage">
-            <h1>Visit our resort and change your life!</h1>
-
-            <h1>Grand opening in StressVille, Texas</h1>
-            
-            <h1>Check out our wide array of packages and offers!</h1>
-
+            <Container>
+                <div className="jumbotron">
+                    <h1>Visit our resort and change your life!</h1>
+                    <h1>Grand opening in StressVille, Texas</h1>
+                    <h1>Check out our wide array of packages and offers!</h1>
+                </div>
+            </Container>
         </div>
-    )
-
+    );
 }
