42b0d7655fbc9179ace126288c7f121e81d4c361
CodWurl <46231725+CodWurl@users.noreply.github.com>
2023-09-15 10:06:20 -0400

    All current changes from babel

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ export default function Home() {
     return (
         <div className="homepage">
             <Container>
-                <div className="jumbotron">
+                <div className="jumbotron" style={{backgroundColor:'rgba(255, 255, 255, 0.3)' }}>
                     <h1>Visit our resort and change your life!</h1>
-                    <h1>Grand opening in StressVille, Texas</h1>
+                    <h1>Grand opening in Charlotte, NC</h1>
                     <h1>Check out our wide array of packages and offers!</h1>
                 </div>
             </Container>
